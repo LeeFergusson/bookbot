adcea96a3339d74f906de74c3cb24c3e2349d51b
Lee Fergusson <lee.fergusson@googlemail.com>
2024-12-23 12:52:31 +0000

    feat: add function for counting unique characters in a string

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     contents = read_file("books/frankenstein.txt")
     print(contents)
     print("Word Count: ", count_words(contents))
+    print("Unique Characters: ", count_unique_characters(contents))
 
 def read_file(file_path):
     """Reads a file and returns the contents as a string"""
@@ -14,5 +15,20 @@ def read_file(file_path):
 def count_words(string):
     """Counts the number of words in a sting and returns the count"""
     return len(string.split())
+
+def count_unique_characters(string):
+    """ Counts the number of unique characters in a string and returns a
+    dictionary with the counts
+    """
+    result = {}
+
+    for character in string:
+        lower_character = character.lower()
+        if lower_character in result:
+            result[lower_character] += 1
+        else:
+            result[lower_character] = 1
+
+    return result
 
 main()
