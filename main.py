794d228393c3f35c25744e99da66d2b36b7d94c4
Lee Fergusson <lee.fergusson@googlemail.com>
2024-12-23 11:15:16 +0000

    chore: add application skeleton

---
+++ main.py
@@ -0,0 +1,11 @@
+# Project: bookbot
+# Description: Main file for the project
+# Author: Lee Fergusson
+
+# Main entry point for the bookbot project
+def main():
+    pass
+
+
+# Run the main function
+main()
