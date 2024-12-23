9e9c4ef5d15a6f13b38e111873efc09a3df69eaf
Lee Fergusson <lee.fergusson@googlemail.com>
2024-12-23 12:24:24 +0000

    feat: add function for reading files

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 
 def main():
     """Main entry point for the bookbot project"""
-    print("Hello, world!")
+    contents = read_file("books/frankenstein.txt")
+    print(contents)
 
+def read_file(file_path):
+    """Reads a file and returns the contents as a string"""
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
 
 main()
