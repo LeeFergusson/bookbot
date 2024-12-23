071a2dadffa92a18fd1bb626f9ec57a43682ba9d
Lee Fergusson <lee.fergusson@googlemail.com>
2024-12-23 12:35:29 +0000

    feat: add function for counting words in a string

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ def main():
     """Main entry point for the bookbot project"""
     contents = read_file("books/frankenstein.txt")
     print(contents)
+    print("Word Count: ", count_words(contents))
 
 def read_file(file_path):
     """Reads a file and returns the contents as a string"""
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read()
+
+def count_words(string):
+    """Counts the number of words in a sting and returns the count"""
+    return len(string.split())
 
 main()
